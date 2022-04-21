2e1f98aa41902fe80fc349894ee0bc67ec85fa5a
vijayaratnakumari <vijaya@sarithm.com>
2022-04-21 17:12:34 +0530

    oops class program

---
+++ 19thclass oops1.py
@@ -0,0 +1,16 @@
+
+class StudentDetials:
+    def __init__(self):
+        self.sid =1234
+        self.sname= "ratna"
+        self.saddress= "mulapalem"
+    def display(self):
+        print("student id is:", self.sid)
+        print("stident name is:", self.sname)
+        print("student address is:", self.saddress)
+
+s1=StudentDetials()
+s1.display()
+s2=StudentDetials()
+s2.display()
+
---
+++ oops.py
@@ -0,0 +1,10 @@
+class Student:
+    '''this is a student class which is used to display the student details
+    '''
+
+#option1
+s=Student()
+print(s.__doc__)
+
+#option 2
+#help(Student)
