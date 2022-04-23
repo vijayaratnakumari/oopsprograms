119712960446522c122098b7db7ad68c9b5dd902
vijayaratnakumari <vijaya@sarithm.com>
2022-04-23 18:58:06 +0530

    Pandas and matplotlib class this is last class in python.

--- 42class last class matplotlib in python.py
+++ 42class last class matplotlib in python.py
@@ -1,10 +1,13 @@
 from matplotlib import pyplot as plt
 
 # x-axis values
+
 x = [5, 2, 1, 4, 7]
 # Y-axis values
+
 y = [10, 5, 8, 4, 2]
 # Function to plot
+
 plt.plot(x,y)
 # function to show the plot
 plt.show()
